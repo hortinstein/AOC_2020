c946c814d3c4fcb27108c5d91efcfac93fec4c9d
Alex Hortin <hortinstein@gmail.com>
2020-12-04 03:32:10 +0000

    wtf rewrote this as garbage

--- d3_python/part1.py
+++ d3_python/part1.py
@@ -6,26 +6,26 @@ import shutil, errno
 
 f = open('input','r')
 
+def count_trees(slope,right,down):
+    x = 0
+    tree_count = 0
+
+    for slope_slice in range(len(slope)):
+        if down != 1 and slope_slice %2 != 0:
+            continue
+        len_slice = len(slope[slope_slice])-1
+        slope_expand = slope[slope_slice].strip() *1000
+        for slope_square in range(len(slope_expand)):
+            if slope_square == x:
+                if slope_expand[x] == "#":
+                    tree_count +=1
+        x+=right
+    
+    print tree_count
+    return tree_count
+    
+    
+
 slope = f.readlines()
-
-x = 0
-y = 0
-tree_count = 0
-
-for slope_slice in slope:
-    len_slice = len(slope_slice)-1
-    for slope_square in range(len(slope_slice)):
-        if slope_square == x % len_slice:
-            if slope_slice[x % len_slice] == "#":
-                tree_count +=1
-                print 'X',
-            else:
-                print '0',
-        else:
-            print slope_slice[slope_square],
-    print x % len_slice
-    x+=3
-    
-print tree_count
-    
-    
+print count_trees(slope,1,1) * count_trees(slope,3,1) * \
+    count_trees(slope,5,1) * count_trees(slope,7,1) * count_trees(slope,1,2)
